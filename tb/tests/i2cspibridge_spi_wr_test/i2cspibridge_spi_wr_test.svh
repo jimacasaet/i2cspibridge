291f6a65a33e74c4a3f5d488a62d56758a499030
John Rufino Macasaet <j_macasaet@vtech-inc.co.jp>
2024-10-29 18:01:08 +0900

    Start on SPI Wr Test

--- tb/tests/i2cspibridge_spi_wr_test/i2cspibridge_spi_wr_test.svh
+++ tb/tests/i2cspibridge_spi_wr_test/i2cspibridge_spi_wr_test.svh
@@ -2,25 +2,25 @@
 // Create Date  :   2024-10-09
 // Author       :   John Rufino Macasaet
 // E-Mail       :   j_macasaet@vtech-inc.co.jp
-// File Name    :   i2cspibridge_sanity_test.svh
+// File Name    :   i2cspibridge_spi_wr_test.svh
 // Description  :   I2C-SPI Bridge Sanity Test
 //-------------------------------------------------------------
-`ifndef _I2CSPIBRIDGE_SANITY_TEST_SVH
-  `define _I2CSPIBRIDGE_SANITY_TEST_SVH
+`ifndef _I2CSPIBRIDGE_SPI_WR_TEST_SVH
+  `define _I2CSPIBRIDGE_SPI_WR_TEST_SVH
 
-class i2cspibridge_sanity_test extends i2cspibridge_base_test;
+class i2cspibridge_spi_wr_test extends i2cspibridge_base_test;
 
   // Instantiate Test Sequence
-  rand i2cspibridge_sanity_test_seq sanity_test_seq;
+  rand i2cspibridge_spi_wr_test_seq spi_wr_test_seq;
 
-  `uvm_component_utils_begin(i2cspibridge_sanity_test)
-    `uvm_field_object(sanity_test_seq, UVM_ALL_ON)
+  `uvm_component_utils_begin(i2cspibridge_spi_wr_test)
+    `uvm_field_object(spi_wr_test_seq, UVM_ALL_ON)
   `uvm_component_utils_end
 
   /******************************************************************************
   *   FUNCTION: Constructor
   ******************************************************************************/
-  function new(string name="i2cspibridge_sanity_test", uvm_component parent=null);
+  function new(string name="i2cspibridge_spi_wr_test", uvm_component parent=null);
     super.new(name, parent);
   endfunction : new
 
@@ -37,23 +37,23 @@ class i2cspibridge_sanity_test extends i2cspibridge_base_test;
   virtual task main_phase(uvm_phase phase);
     super.main_phase(phase);
     // Create the test seq
-    sanity_test_seq = i2cspibridge_sanity_test_seq::type_id::create("sanity_test_seq", this);
+    spi_wr_test_seq = i2cspibridge_spi_wr_test_seq::type_id::create("spi_wr_test_seq", this);
 
     // Pass the configuration object to test seq
-    sanity_test_seq.m_cfg = m_cfg;
+    spi_wr_test_seq.m_cfg = m_cfg;
 
     // Start test and raise objection
     `uvm_info("I2CSPIBRIDGE Sanity Test", "Starting Test...", UVM_LOW)
     phase.raise_objection(this);
       // Randomize the test sequence
-      assert(sanity_test_seq.randomize())
+      assert(spi_wr_test_seq.randomize())
       else
         `uvm_error("I2CSPIBRIDGE Sanity Test", "Failed to randomize sequence")
       // Start Test Seq
-      sanity_test_seq.start(t_env.m_vseqr);
+      spi_wr_test_seq.start(t_env.m_vseqr);
     phase.drop_objection(this);
   endtask : main_phase
 
 
-endclass : i2cspibridge_sanity_test
-`endif // _I2CSPIBRIDGE_SANITY_TEST_SVH
+endclass : i2cspibridge_spi_wr_test
+`endif // _I2CSPIBRIDGE_SPI_WR_TEST_SVH
