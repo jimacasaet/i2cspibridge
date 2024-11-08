c797c66441e01d9a996732b407ca12d66f765fbc
John Rufino Macasaet <j_macasaet@vtech-inc.co.jp>
2024-11-08 17:47:37 +0900

    Added RegMap Wr Test

--- tb/tests/i2cspibridge_regmap_wr_test/i2cspibridge_regmap_wr_test_seq.svh
+++ tb/tests/i2cspibridge_regmap_wr_test/i2cspibridge_regmap_wr_test_seq.svh
@@ -2,16 +2,16 @@
 // Create Date  :   2024-10-25
 // Author       :   John Rufino Macasaet
 // E-Mail       :   j_macasaet@vtech-inc.co.jp
-// File Name    :   i2cspibridge_spi_wr_test_seq.svh
-// Description  :   I2C-SPI Bridge SPI Write Test Sequence
+// File Name    :   i2cspibridge_regmap_wr_test_seq.svh
+// Description  :   I2C-SPI Bridge RegMap Write Test Sequence
 //-------------------------------------------------------------
-`ifndef _I2CSPIBRIDGE_SPI_WR_TEST_SEQ_SVH
-  `define _I2CSPIBRIDGE_SPI_WR_TEST_SEQ_SVH
+`ifndef _I2CSPIBRIDGE_REGMAP_WR_TEST_SEQ_SVH
+  `define _I2CSPIBRIDGE_REGMAP_WR_TEST_SEQ_SVH
 
-class i2cspibridge_spi_wr_test_seq extends i2cspibridge_base_seq;
+class i2cspibridge_regmap_wr_test_seq extends i2cspibridge_base_seq;
 
   // Register with the factory
-  `uvm_object_param_utils_begin(i2cspibridge_spi_wr_test_seq)
+  `uvm_object_param_utils_begin(i2cspibridge_regmap_wr_test_seq)
   `uvm_object_utils_end
 
   // Parameters
@@ -20,9 +20,6 @@ class i2cspibridge_spi_wr_test_seq extends i2cspibridge_base_seq;
 
   // Randomization variables
   rand i2c_freq_e                 i2c_freq;
-  rand logic  [1:0]               spi_slave;
-  rand logic                      spi_clk;
-  rand logic  [1:0]               spi_cfg;
   rand logic  [WR_ADR_WIDTH-1:0]  wr_addr;
   rand int                        iter;
   rand logic                      reset_bridge;
@@ -35,9 +32,9 @@ class i2cspibridge_spi_wr_test_seq extends i2cspibridge_base_seq;
   /************************************************************
   *   FUNCTION: Constructor
   *************************************************************/
-  function new(string name="i2cspibridge_spi_wr_test_seq");
+  function new(string name="i2cspibridge_regmap_wr_test_seq");
     super.new(name);
-    `uvm_info("SPI Write Test Seq","Constructor Done", UVM_HIGH)
+    `uvm_info("RegMap Write Test Seq","Constructor Done", UVM_HIGH)
   endfunction : new
 
   /************************************************************
@@ -51,7 +48,7 @@ class i2cspibridge_spi_wr_test_seq extends i2cspibridge_base_seq;
     
     super.body();
 
-    // Randomized SPI Write Test: Loop stimulus until timeout occurs
+    // Randomized RegMap Write Test: Loop stimulus until timeout occurs
     
     fork
       // Thread for stimulus
@@ -59,26 +56,24 @@ class i2cspibridge_spi_wr_test_seq extends i2cspibridge_base_seq;
         // 
         forever begin
           // Step 1: Randomize test parameters
-          `uvm_info("SPI WR TEST SEQ", "Randomizing Parameters", UVM_MEDIUM)
+          `uvm_info("RegMap WR TEST SEQ", "Randomizing Parameters", UVM_MEDIUM)
           randomize_test_params();
 
           // Step 2&3: Configure the RegMap and Send Stop Condition
-          `uvm_info("SPI WR TEST SEQ", "Configuring RegMap", UVM_MEDIUM)
-          config_seq.dest     = spi_slave;
-          config_seq.spi_clk  = spi_clk;
-          config_seq.spi_cfg  = spi_cfg;
+          `uvm_info("RegMap WR TEST SEQ", "Configuring RegMap", UVM_MEDIUM)
+          config_seq.dest     = 0;
           config_seq.start(p_sequencer);
 
           // Do the write
-          `uvm_info("SPI WR TEST SEQ", "Writing to SPI SLV", UVM_MEDIUM)
-          write_data_seq.addr       = {spi_slave, wr_addr};
+          `uvm_info("RegMap WR TEST SEQ", "Writing to RegMap", UVM_MEDIUM)
+          write_data_seq.addr       = {2'h0, wr_addr};
           write_data_seq.write_iter = iter;
           write_data_seq.start(p_sequencer);
-          `uvm_info("SPI WR TEST SEQ", "Finished Write", UVM_MEDIUM)
+          `uvm_info("RegMap WR TEST SEQ", "Finished Write", UVM_MEDIUM)
           
           // Reset the I2C-SPI Bridge
           if(reset_bridge) begin
-            `uvm_info("SPI WR TEST SEQ", "Resetting I2C-SPI Bridge", UVM_MEDIUM)
+            `uvm_info("RegMap WR TEST SEQ", "Resetting I2C-SPI Bridge", UVM_MEDIUM)
           end
           change_scl_freq(I2C_STANDARD_MODE);
         end
@@ -87,7 +82,7 @@ class i2cspibridge_spi_wr_test_seq extends i2cspibridge_base_seq;
       // Thread for timeout
       begin : timeout_th
         #(TEST_TIMEOUT );
-        `uvm_info("SPI WR TEST SEQ", "Test Ended!", UVM_MEDIUM)
+        `uvm_info("RegMap WR TEST SEQ", "Test Ended!", UVM_MEDIUM)
       end   : timeout_th
     join_any
   endtask : body
@@ -99,17 +94,14 @@ class i2cspibridge_spi_wr_test_seq extends i2cspibridge_base_seq;
     assert(randomize(i2c_freq));
     assert(randomize(wr_addr) with { wr_addr inside {[0:63]}; } );
     assert(randomize(iter) with {iter inside {[0:64]}; } );
-    assert(randomize(spi_slave) with { spi_slave inside {2'b01, 2'b10, 2'b11}; } );
-    assert(randomize(spi_clk) with {spi_clk dist {1'b0:= 70, 1'b1 := 30}; } );
-    assert(randomize(spi_cfg) with {spi_cfg dist {2'b00:=60, [2'b01:2'b11]:=0}; } );
     assert(randomize(reset_bridge) with {reset_bridge dist {1'b0 := 60, 1'b1 := 40}; });
     //Change the I2C Clock Frequency
     change_scl_freq(i2c_freq);
     //Print the randomized parameters
-    `uvm_info("SPI WR Test Seq", $sformatf("Rand Test Params\ni2c_freq=%0s\nwr_addr=%0h\niter=%0d\nspi_slv=%0b\nspi_clk=%0h\nspi_cfg=%0b\nreset_bridge=%0s",
-              i2c_freq.name, wr_addr, iter, spi_slave, spi_clk, spi_cfg, (reset_bridge?"TRUE":"FALSE")), UVM_LOW)
+    `uvm_info("RegMap WR Test Seq", $sformatf("Rand Test Params\ni2c_freq=%0s\nwr_addr=%0h\niter=%0d\nreset_bridge=%0s",
+              i2c_freq.name, wr_addr, iter,(reset_bridge?"TRUE":"FALSE")), UVM_LOW)
   endtask : randomize_test_params
   
-endclass : i2cspibridge_spi_wr_test_seq
+endclass : i2cspibridge_regmap_wr_test_seq
 
-`endif // _I2CSPIBRIDGE_SPI_WR_TEST_SEQ_SVH
+`endif // _I2CSPIBRIDGE_REGMAP_WR_TEST_SEQ_SVH
